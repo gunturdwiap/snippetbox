fd229e8b657eca7c08649e3e7543e5a078d9358f
gunturdwiap <kadekguntur13@gmail.com>
2026-04-10 21:45:44 +0800

    context

--- internal/models/users.go
+++ internal/models/users.go
@@ -70,5 +70,10 @@ func (m *UserModel) Authenticate(email, password string) (int, error) {
 }
 
 func (m *UserModel) Exists(id int) (bool, error) {
-	return false, nil
+	var exists bool
+
+	stmt := `SELECT EXIST(SELECT true FROM users where id = ?)`
+
+	err := m.DB.QueryRow(stmt, id).Scan(&exists)
+	return exists, err
 }
